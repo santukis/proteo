57faa66a2efdda626bbd73ea66f537575b6489c3
David <santamadavid@gmail.com>
2021-10-20 10:15:39 +0200

    created StatefulRecyclerView.kt

---
+++ proteo/src/main/java/com/frikiplanet/proteo/StatefulRecyclerView.kt
@@ -0,0 +1,42 @@
+package com.frikiplanet.proteo
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class StatefulRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null): RecyclerView(context, attributeSet) {
+
+    private val LAYOUT_MANAGER_STATE = "LAYOUT_MANAGER_STATE"
+
+    private var state: Parcelable? = null
+
+    override fun onSaveInstanceState(): Parcelable? {
+        super.onSaveInstanceState()
+        return Bundle().apply {
+            putParcelable(LAYOUT_MANAGER_STATE, layoutManager?.onSaveInstanceState())
+        }
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        (state as? Bundle)?.let { bundle ->
+            this.state = bundle.getParcelable(LAYOUT_MANAGER_STATE)
+        }
+        super.onRestoreInstanceState(state)
+    }
+
+    override fun setAdapter(adapter: Adapter<*>?) {
+        super.setAdapter(adapter)
+        restorePosition()
+    }
+
+    private fun restorePosition() {
+        state?.let { savedState ->
+            layoutManager?.onRestoreInstanceState(savedState)
+            state = null
+        }
+    }
+}
